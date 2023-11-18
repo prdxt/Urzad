c3faf0c21c1adafeb732832e0aed26f81538f2c2
prdxt <dominosjaj@gmail.com>
2023-11-18 12:55:52 +0100

    Dodanie funkcjonalności do klas Documents i Marriage (Klasy i tak nie działają tak jak powinny)

--- src/entities/Documents.java
+++ src/entities/Documents.java
@@ -4,4 +4,26 @@ public class Documents {
     private String documentType;
     private String documentContent;
 
+    public Documents(String documentType, String documentContent) {
+        this.documentType = documentType;
+        this.documentContent = documentContent;
+    }
+
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+
+    public String getDocumentContent() {
+        return documentContent;
+    }
+
+    public void setDocumentContent(String documentContent) {
+        this.documentContent = documentContent;
+    }
 }
+
--- src/entities/Marriage.java
+++ src/entities/Marriage.java
@@ -1,6 +1,7 @@
 package entities;
 
-import entities.Person;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Marriage {
     private Person spouse1;
@@ -13,5 +14,46 @@ public class Marriage {
         this.marriageDate = marriageDate;
     }
 
+    public void performCivilMarriage(List<Documents> documentsCollection) {
+        if (spouse1 != null && spouse2 != null) {
+            String marriageCertificate = String.format("Akt ślubu między %s a %s, zawarty dnia %s.",
+                    spouse1.getFirstName() + " " + spouse1.getLastName(),
+                    spouse2.getFirstName() + " " + spouse2.getLastName(),
+                    marriageDate);
 
+            Documents marriageDocument = new Documents("Marriage Certificate", marriageCertificate);
+
+            documentsCollection.add(marriageDocument);
+
+            System.out.println("Ślub cywilny został zarejestrowany, dodano akt ślubu do dokumentów.");
+        } else {
+            System.out.println("Nie można zarejestrować ślubu. Sprawdź poprawność informacji o małżonkach.");
+        }
+    }
+
+
+
+    public Person getSpouse1() {
+        return spouse1;
+    }
+
+    public void setSpouse1(Person spouse1) {
+        this.spouse1 = spouse1;
+    }
+
+    public Person getSpouse2() {
+        return spouse2;
+    }
+
+    public void setSpouse2(Person spouse2) {
+        this.spouse2 = spouse2;
+    }
+
+    public String getMarriageDate() {
+        return marriageDate;
+    }
+
+    public void setMarriageDate(String marriageDate) {
+        this.marriageDate = marriageDate;
+    }
 }
